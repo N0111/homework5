d474b84e802dbd3c8bfcfdc8947c4d430b365479
Nastya K <nastulya01112005@gmail.com>
2021-12-31 17:54:41 +0200

    Homework5

---
+++ 1/V1/Program.cs
@@ -0,0 +1,54 @@
+﻿// 1.
+// Создать программу, которая будет
+// проверять корректность ввода логина. 
+// Корректным логином будет
+// строка от 2 до 10 символов,
+// содержащая только буквы латинского
+// алфавита или цифры, при этом 
+// цифра не может быть первой:
+// а) без использования регулярных выражений;
+using System;
+using System.Text;
+
+Console.Write("Введите логин:");
+StringBuilder sb = new StringBuilder(Console.ReadLine());
+bool res = false;
+while (res == false)
+{
+    if (sb.Length > 2 && sb.Length < 10)
+    {
+
+        char a = sb[0];
+        if (char.IsNumber(a) == false)
+        {
+            for (int i = 1; i < sb.Length; i++)
+            {
+                if (char.IsNumber(sb[i]) | char.IsLetter(sb[i]))
+                {
+                    res = true;
+                }
+                else
+                {
+                    res = false;
+                    Console.Write("Логин должен содержать только буквы или цифры . Введите другой:");
+                    sb = new StringBuilder(Console.ReadLine());
+                }
+            }
+
+        }
+        else
+        {
+            Console.Write("Цифра в логине не может быть первой. Введите другой:");
+            sb = new StringBuilder(Console.ReadLine());
+        }
+
+    }
+    else
+    {
+        Console.Write("Логин должен быть больше 2 и меньше 10. Введите другой:");
+        sb = new StringBuilder(Console.ReadLine());
+        res = false;
+    }
+
+}
+Console.Write("Логин отличный, спасибо :-)");
---
+++ 3/Program.cs
@@ -0,0 +1,53 @@
+﻿// 3. 
+// *Для двух строк написать метод, определяющий, 
+// является ли одна строка перестановкой другой.
+// Например: badc являются перестановкой abcd.
+
+using System;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите первую строку: ");
+        String a = new String(Console.ReadLine());
+        Console.Write("Введите вторую строку: ");
+        String b = new String(Console.ReadLine());
+        bool res = false;
+        if (a.Length != b.Length)
+        {
+            res = false;
+
+
+        }
+        else
+        {
+            Char[] m = a.ToCharArray();
+            Char[] k = b.ToCharArray();
+            Array.Sort(m);
+            Array.Sort(k);
+            res = true;
+            for (int i = 0; i < k.Length; i++)
+            {
+                if (m[i] != k[i])
+                {
+                    res = false;
+                    break;
+                }
+
+            }
+
+
+        }
+        if (res)
+        {
+            Console.WriteLine("Да: одна строка является перестановкой другой.");
+        }
+        else
+        {
+            Console.WriteLine("Нет: одна строка не является перестановкой другой.");
+        }
+    }
+
+
+}
